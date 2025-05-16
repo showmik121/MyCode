7a1e5b181047ef81f6ba88564fc94478116c8e63
Ahanaf Adil Showmik <145767052+showmik121@users.noreply.github.com>
2025-05-16 17:56:58 +0600

    + operator

---
+++ OOP/Operator_overloding.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Complex
+{
+    int real, img;
+
+public:
+    Complex() {
+
+    };
+    Complex(int real, int img)
+    {
+        this->real = real;
+        this->img = img;
+    }
+    void display()
+    {
+        cout << real << " +i " << img << endl;
+    }
+    Complex operator+(const Complex &c)
+    {
+        Complex ans;
+        ans.real = real + c.real;
+        ans.img = img + c.img;
+        return ans;
+    }
+};
+int main()
+{
+    Complex c1(3, 4);
+    Complex c2(4, 6);
+    Complex c3 = c1 + c2; // c1.fun(c2)
+    c3.display();
+
+    return 0;
+}
